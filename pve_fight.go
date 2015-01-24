1ed794326e2894eda1fca6d5bd97e6485ff5ef74
John Nelson <jnelson@johncoder.com>
2015-01-24 13:26:52 -0500

    Moved fight function

--- pve_fight.go
+++ pve_fight.go
@@ -29,3 +29,39 @@ func (n *NPCUnit) genNPCStats(h *HeroSheet) {
 	n.Power = random(h.Power-ENEMY_VARIANCE_POWER, h.Power+ENEMY_VARIANCE_POWER)
 	n.Speed = random(h.Speed-ENEMY_VARIANCE_SPEED, h.Speed+ENEMY_VARIANCE_SPEED)
 }
+
+func Fight(p1 *HeroSheet, n *NPCUnit) {
+	// p1 is fighting n
+	// copy base stats to temp
+	// loop and run combat formula
+
+	// Speed, Power, Life
+	// Loop to increment "initiative" by speed, when > hit with power
+
+	p1Life := p1.Life
+	nLife := n.Life
+
+	// Synchronize the following goroutines with channels
+	// select { } with channels
+
+	// stop := make(chan bool)
+
+	// "p1" sim hitting "n", which is just a dummy right now
+	go func() {
+		for nLife > 0 {
+			for p1Initiative := int64(0); p1Initiative < 100; p1Initiative += p1.Speed {
+				// hit!
+				nLife -= p1.Power
+			}
+		}
+	}()
+	// "n" sim hitting "p1"
+	go func() {
+		for p1Life > 0 {
+			for nInitiative := int64(0); nInitiative < 100; nInitiative += n.Speed {
+				// hit!
+				p1Life -= n.Power
+			}
+		}
+	}()
+}
--- pve_fight_test.go
+++ pve_fight_test.go
@@ -22,38 +22,7 @@ func TestEnemyGenStats(t *testing.T) {
 	testSpeed(&e.BaseStats, t, h.BaseStats.Speed-ENEMY_VARIANCE_SPEED, h.BaseStats.Speed+ENEMY_VARIANCE_SPEED)
 }
 
-func Fight(p1 *HeroSheet, n *NPCUnit) {
-	// p1 is fighting n
-	// copy base stats to temp
-	// loop and run combat formula
-
-	// Speed, Power, Life
-	// Loop to increment "initiative" by speed, when > hit with power
-
-	p1Life := p1.Life
-	nLife := n.Life
-
-	// Synchronize the following goroutines with channels
-	// select { } with channels
-
-	// stop := make(chan bool)
-
-	// "p1" sim hitting "n", which is just a dummy right now
-	go func() {
-		for nLife > 0 {
-			for p1Initiative := int64(0); p1Initiative < 100; p1Initiative += p1.Speed {
-				// hit!
-				nLife -= p1.Power
-			}
-		}
-	}()
-	// "n" sim hitting "p1"
-	go func() {
-		for p1Life > 0 {
-			for nInitiative := int64(0); nInitiative < 100; nInitiative += n.Speed {
-				// hit!
-				p1Life -= n.Power
-			}
-		}
-	}()
+func TestFight(t *testing.T) {
+	t.Log("Too peaceful...")
+	t.Fail()
 }
