4cea5b33ad511dc53c5c44193c96366dff920c41
Caleb Gossler <caleb.gossler@gmail.com>
2015-01-25 00:34:57 -0800

    going on an adventure may result in an NPC fight

--- pve_adventure.go
+++ pve_adventure.go
@@ -1,6 +1,8 @@
 package main
 
-import ()
+import (
+	"fmt"
+)
 
 const (
 	ADVENTURE_TYPE_ENCOUNTER AdventureType = 1
@@ -12,12 +14,14 @@ type (
 	AdventureType int
 	Adventure     struct {
 		Type AdventureType
+		*HeroSheet
 	}
 )
 
 func NewAdventure(h *HeroSheet) *Adventure {
 	return &Adventure{
 		Type: generateAdventure(),
+		HeroSheet: h,
 	}
 }
 
@@ -25,11 +29,15 @@ func generateAdventure() AdventureType {
 	return AdventureType(random(1, 3))
 }
 
-func (a *Adventure) Embark() {
+func (a *Adventure) Embark(game func(Event)) {
 	switch a.Type {
 	case ADVENTURE_TYPE_DISCOVERY:
+		fmt.Println("You didn't discover anything, too bad.")
 	case ADVENTURE_TYPE_ENCOUNTER:
+		fmt.Println("A wild Snorlax appeared and you fought!")
+		snorlax := NewEnemy(a.HeroSheet)
+		Fight(a.HeroSheet, snorlax, game)
 	case ADVENTURE_TYPE_WANDER:
-
+		fmt.Println("You wandered right back to where you started")
 	}
 }
